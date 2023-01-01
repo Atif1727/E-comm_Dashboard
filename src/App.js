8d217fba7091d238c992ed3e588620cdb690579a
Atif1727 <ghoghaatif2002@gmail.com>
2023-01-01 10:15:46 +0530

    E-commerce Frontend part

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
+import { BrowserRouter ,Route,Routes} from 'react-router-dom';
+import Footer from './components/Footer';
+import Nav from './components/Nav';
+import SignUp from './components/SignUp';
 import './App.css';
+import PrivateComponent from './components/PrivateComponents';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+      <Nav />
+      <Routes>
+        <Route element={<PrivateComponent/>}>
+        <Route path='/' element={<h1>Products listing Components</h1>} />
+        <Route path='/add' element={<h1>add Products</h1>} />
+        <Route path='/update' element={<h1>update Products</h1>} />
+        <Route path='/logout' element={<h1>Logout</h1>} />
+        <Route path='/profile' element={<h1>Profile</h1>} />
+        </Route>
+
+        <Route path='/signup' element={<SignUp />} />
+
+      </Routes>
+      </BrowserRouter>
+      <Footer />
     </div>
   );
 }
